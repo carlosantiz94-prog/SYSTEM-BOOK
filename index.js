5ee62194a4382000da8a59108a3fdbe35d79f2d8
Carlos Santiz <carlosantiz94@gmail.com>
2025-11-10 19:22:48 -0500

    metodos array

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // DECLARAR LA COLECCION DE LIBROS
-const books = [];
+const books = [
+  { title: "Cien Años de Soledad", author: "Gabriel García Márquez", year: 1967 },
+];
 ``
 // FUNCION PARA AGREGAR LIBROS
 const addBook = (title, author, year) => {
@@ -34,11 +36,57 @@ const deleteBook = (title) => {
     console.log("Libro no encontrado para eliminar.");
   }
 };
-// PRUEBA BASICA
-addBook("Cien Años de Soledad", "Gabriel García Márquez", 1967);
-addBook("El Principito", "Antoine de Saint-Exupéry", 1943);
-listBooks();
-findBook("El Principito");
-deleteBook("Cien Años de Soledad");
-listBooks();
+// METODO MAP
+const title = books.map(book => book.title);
+  return book.title;
+
+  console.log(title);
 ``
+// METODO FILTER
+const libroscaros = libros.filter((libro) => {
+  return libro.precio > 20;
+
+  console.log(libroscaros)
+
+  const librosFormato = libros.filter((libro) =>{
+    return libro.formato === "tapa blanda"
+})
+  console.log(librosFormato.length)
+});
+
+// METODO REDUCE
+const total = libros.reduce((acc, libro) => {
+    return total + libro.precio;
+}, 0);
+console.log(total);
+
+// METODO FIND
+const libro = libros.find((libro) => {
+    return libro.idioma === "español";
+    console.log(libroTolkien)
+});
+
+// METODO SOME
+const algunos = libros.some((libro) => {
+    return libro.idioma === "español";
+    console.log(algunos)
+});
+
+// METODO EVERY
+const todos = libros.every((libro) => {
+    return libro.idioma === "español";
+    console.log(todos)
+});
+
+// METODO SLICE
+const librosPorFormato = libros.slice(0, 2);
+console.log(librosPorFormato);
+
+// METODO SORT
+const librosOrdenados = libros.sort((a, b) => {
+    return a.precio - b.precio;
+});
+console.log(librosOrdenados);
+
+
+
